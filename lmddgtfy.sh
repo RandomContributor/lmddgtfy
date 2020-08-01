880717a3b48d93d62868f475e15da0d219fe2402
RandomContributor <fuzzybyte@national.shitposting.agency>
2020-08-02 01:12:10 +0200

    more engines, comment

--- lmddgtfy.sh
+++ lmddgtfy.sh
@@ -20,11 +20,43 @@ function out::suc {
 }
 ###
 
+# Protocols
+# 0 <~> https
+# 1 <~> http
+
 PROTO=0
 PROTOS=("https://" "http://")
 
+# Search services
+# 0 <~> DDG
+# 1 <~> DDG Lite
+# 2 <~> PrivateNet Searx
+# 3 <~> Google
+# 4 <~> StartPage
+# 5 <~> Bing
+# 6 <~> Qwant
+# 7 <~> Yahoo
+# 8 <~> Yandex
+# 9 <~> Baidu
+#10 <~> Ecogine
+
 URL=0
-URLS=("duckduckgo.com?q=" "duckduckgo.com/lite?q=" "google.com/search?q=")
+URLS=\
+(
+'duckduckgo.com?q='
+'duckduckgo.com/lite?q='
+'searx.privatenet.cf?categories=general&q='
+'google.com/search?q='
+'https://www.startpage.com/do/dsearch?query='
+'bing.com/search?q='
+'qwant.com?q='
+'search.yahoo.com/search?p='
+'yandex.com/search?text='
+'baidu.com/s?wd='
+'ecogine.org?q='
+)
+
+# Final URL
 
 FINAL="${PROTOS[$PROTO]}${URLS[$URL]}$(tr " " "+" <<<"${1}")"
 
